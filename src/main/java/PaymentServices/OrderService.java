f17c8ad6d95c079a0c6a40a4df2c53c8162a9cdd
Shayan Shabanzadeh <shayan.shabanzadeh@peykasa.ir>
2023-11-09 17:20:34 +0330

    refactor: add two function for phone order service in order service

--- src/main/java/PaymentServices/OrderService.java
+++ src/main/java/PaymentServices/OrderService.java
@@ -2,8 +2,16 @@ package PaymentServices;
 
 public interface OrderService {
     public void onSiteOrderRegister(String customerName);
+
     public void onlineOrderRegister(String customerName);
+
+    void phoneOrderRegister(String customerName);
+
     public void onSiteOrderPayment(int foodPrice);
+
     public void onlineOrderPayment(int foodPrice);
+
+
+    void phoneOrderPayment(int foodPrice);
 
 }
